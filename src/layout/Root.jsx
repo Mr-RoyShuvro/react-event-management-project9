f0bd684315632630fda830b380d21239162710a5
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-06-07 21:32:44 +0600

    added font-family and navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../pages/Shared/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="font-roboto max-w-6xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
